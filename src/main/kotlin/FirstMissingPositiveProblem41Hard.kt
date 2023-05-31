d99e090b561a9a12e746cdadc2e95c07c70095c8
unknown <daviti.okruashvili.1@btu.edu.ge>
2023-05-31 19:35:43 +0400

    added Problem N41 (Hard)

---
+++ src/main/kotlin/FirstMissingPositiveProblem41Hard.kt
@@ -0,0 +1,20 @@
+fun main(){
+    val arr = intArrayOf()
+    println(firstMissingPositive(arr))
+}
+
+
+fun firstMissingPositive(nums: IntArray): Int {
+
+
+    val arr = nums.filter { it > 0 }.distinct().sorted()
+    if (arr.isEmpty()) return 1
+    var i = 1
+    while (i <= arr.size){
+        if (i < arr[i - 1]){
+            return i
+        }
+        i++
+    }
+    return arr.last() + 1
+}
